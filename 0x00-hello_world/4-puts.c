7e1af097b71f27b12f7be1aa1d97174eeb8f64ad
Abdikhalaqahmad1415 <abdikhalaqahmad1415@gmail.com>
2022-09-08 15:34:32 -0700

    my

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
+ * main - entry point
  *
- * return: 0
+ * return: 0 on success
  */
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+{puts("\"Programming is like building a multilingual puzzle");
+	return (0);}
