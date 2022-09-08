dd239ce5d40b3d8d8954c7baed05c0be609f45f6
Abdikhalaqahmad1415 <abdikhalaqahmad1415@gmail.com>
2022-09-08 16:06:41 -0700

    my

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,4 @@
  *Return: 0 on success
  */
 int main(void)
-{
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
-}
+{puts("\"Programming is like building a multilingual puzzle");return (0);}
