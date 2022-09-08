6c8e01d98166d1cc2d25996d7924736832c3161d
Abdikhalaqahmad1415 <abdikhalaqahmad1415@gmail.com>
2022-09-08 14:00:32 -0700

    my

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+
+/**
+ * main - Printing
+ *
+ * return: always 0 (succesful)
+ */
+int main(viod)
+{puts("\"programming is like biulding a multilingual puzzle");
+	return (0);}
