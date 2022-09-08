da9c9dd18c0c8d3a8a4dae6eba6ea531159dcf7b
Abdikhalaqahmad1415 <abdikhalaqahmad1415@gmail.com>
2022-09-08 15:37:35 -0700

    my

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
+
+
 /**
- * main - entry point
  *
- * return: 0 on success
- */
+ *   * main - Entry point
+ *
+ *     *
+ *
+ *       * Return: 0 on success
+ *
+ *         */
+
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-	return (0);}
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
