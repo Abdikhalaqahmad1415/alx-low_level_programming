6d1828c4a0a45845f79bae242699335e06a1863a
Abdikhalaqahmad1415 <abdikhalaqahmad1415@gmail.com>
2022-09-09 13:18:46 -0700

    my

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * return: 0 0n success
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	for (; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+		if (letter == 'z')
+			putchar('\n');
+	}
+	return (0);
+}
