c479943887dceacbf8d233b562c60c7de67b77d7
Abdikhalaqahmad1415 <abdikhalaqahmad1415@gmail.com>
2022-09-09 14:18:56 -0700

    my

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * maim - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int num = 48;
+
+	for (; num < 58 ; num++)
+	{
+		putchar(num);
+		if (num < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		if (num == 57)
+			putchar('\n');
+	}
+	return (0);
+}
