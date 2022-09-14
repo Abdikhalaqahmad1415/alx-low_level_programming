aa4353a65a83eb34eecbeaa52f30b0b32e6902c8
Abdikhalaqahmad1415 <abdikhalaqahmad1415@gmail.com>
2022-09-14 15:34:46 -0700

    my

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,13 +15,13 @@ void jack_bauer(void)
 	{
 	while (minutes <= 59)
 	{
-	mins_remainder = minutes % 10;
-	hours_remainder = hours % 10;
+	mins_remainders = minutes % 10;
+	hours_remainders = hours % 10;
         _putchar(hours / 10 + '0');
 	_putchar(hours_remainders + '0');
 	_putchar(':');
 	_putchar(minutes / 10 + '0');
-	_putchar(mins_remainder + '0');
+	_putchar(mins_remainders + '0');
 	minutes++;
 	_putchar('\n');
 	}
